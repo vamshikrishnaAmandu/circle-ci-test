8bd992db2f675cc11440dbd31734d63803ff3418
krishna <krishna@gmail.com>
2020-08-18 16:41:57 +0530

    Initial Commit

---
+++ mssc-brewery-mapstruct-example/src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    @Null
+    private UUID id;
+
+    @NotBlank
+    private String beerName;
+
+    @NotBlank
+    private String beerStyle;
+
+    @Positive
+    private Long upc;
+}
